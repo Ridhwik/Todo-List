b915fd85bd6a693a3ce38b79123988b3e1f19b4e
Syfer <jridhwik@gmail.com>
2024-12-16 01:39:45 +0530

    Initial commit

--- ToDoList/src/main/java/com/project/ToDoList/ToDoController/ToDoController.java
+++ ToDoList/src/main/java/com/project/ToDoList/ToDoController/ToDoController.java
@@ -1,10 +1,13 @@
 package com.project.ToDoList.ToDoController;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.project.ToDoList.ToDo;
@@ -26,6 +29,26 @@ public class ToDoController {
 		
 		return "todo";
 	}
+	
+	@RequestMapping(value = "add-todo" , method = RequestMethod.GET)
+	public String showaddTodoPage() {
+		
+		
+		
+		return "Addtodo";
+	}
+	
+	@RequestMapping(value = "add-todo" , method = RequestMethod.POST)
+	public String addnewTodo(@RequestParam String description , ModelMap model) {
+		
+		String name = (String) model.get("name");
+		
+		
+		todoService.addTodo(name, description, LocalDate.now().plusDays(10), false);
+		
+		return "redirect:list-todo";
+	}
+	
 	
 	
 	
--- ToDoList/src/main/java/com/project/ToDoList/ToDoService.java
+++ ToDoList/src/main/java/com/project/ToDoList/ToDoService.java
@@ -7,15 +7,17 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 
+
 @Service
 public class ToDoService {
 	
 	private static List<ToDo> todo  = new ArrayList<>();
+	public static int CountID = 0;
 	
 	static {
-		todo.add(new ToDo(1, "Syfer" , "Learning SpringBoot" , LocalDate.now().plusYears(1) , false));
-		todo.add(new ToDo(2, "Syfer" , "Learning AWS" , LocalDate.now().plusYears(2) , false));
-		todo.add(new ToDo(3, "Syfer" , "Learning Development" , LocalDate.now().plusYears(3) , false));
+		todo.add(new ToDo(++CountID, "Syfer" , "Learning SpringBoot" , LocalDate.now().plusYears(1) , false));
+		todo.add(new ToDo(++CountID, "Syfer" , "Learning AWS" , LocalDate.now().plusYears(2) , false));
+		todo.add(new ToDo(++CountID, "Syfer" , "Learning Development" , LocalDate.now().plusYears(3) , false));
 		
 	}
 	
@@ -23,5 +25,14 @@ public class ToDoService {
 		
 		return todo;
 	}
+	
+	public void addTodo(String UserName , String description , LocalDate localdate, boolean done){
+		ToDo TODO = new ToDo(++CountID, UserName, description, localdate, done);
+		
+		todo.add(TODO);
+		
+	}
+
+	
 
 }
