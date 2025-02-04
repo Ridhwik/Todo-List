e27a6f82343432bdf90f6521726882cb5947e321
Syfer <jridhwik@gmail.com>
2025-02-05 00:34:39 +0530

    Done

--- ToDoList/src/main/java/com/project/ToDoList/Login/WelcomeController.java
+++ ToDoList/src/main/java/com/project/ToDoList/Login/WelcomeController.java
@@ -1,10 +1,10 @@
 package com.project.ToDoList.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -15,9 +15,15 @@ public class WelcomeController {
 	@RequestMapping(value="/")
 	public String gotoWelcomePage(ModelMap model) {
 		
-		model.put("name", "Syfer");
+		model.put("name", getLoggedIn());
 		
 		return "welcome";
+	}
+	
+	
+	private String getLoggedIn() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 
 	
--- ToDoList/src/main/java/com/project/ToDoList/security/SpringSecurityConfiguration.java
+++ ToDoList/src/main/java/com/project/ToDoList/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.project.ToDoList.security;
 
 import java.util.function.Function;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -18,16 +22,45 @@ public class SpringSecurityConfiguration {
 	public InMemoryUserDetailsManager createUserDetailManager() {
 		
 		
-		Function<String, String> passEncoder = input -> passwordEncoder().encode(input);
 		
-		UserDetails userDetails = User.builder().passwordEncoder(passEncoder)
-									.username("Syfer")
-									.password("Ramzi")
-									.roles("USER", "ADMIN")
-									.build();
+		UserDetails userDetails = createNewUser("Syfer", "Ramzi");
+		
+		
 		
 		return new InMemoryUserDetailsManager(userDetails);
 	}
+
+
+
+	private UserDetails createNewUser(String username, String password) {
+		Function<String, String> passEncoder = input -> passwordEncoder().encode(input);
+
+		UserDetails userDetails = User.builder().passwordEncoder(passEncoder)
+									.username(username)
+									.password(password)
+									.roles("USER", "ADMIN")
+									.build();
+		return userDetails;
+	}
+	
+	//All URLs are protected
+		//A login form is shown for unauthorized requests
+		//CSRF disable
+		//Frames
+		
+		@SuppressWarnings("removal")
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			
+			http.authorizeHttpRequests(
+					auth -> auth.anyRequest().authenticated());
+			http.formLogin(withDefaults());
+			
+			http.csrf().disable();
+			http.headers().frameOptions().disable();
+			
+			return http.build();
+		}
 	
 	
 	
